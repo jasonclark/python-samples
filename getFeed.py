e5db3853b96643ae4ee188e95004f75b772fcc86
Jason A. Clark <jasclark@gmail.com>
2016-05-21 20:42:39 -0600

    initial commit

---
+++ getFeed.py
@@ -0,0 +1,31 @@
+# get feed data from url
+import urllib
+import sys
+import xml.dom.minidom
+
+# URI of the feed
+URI = 'http://twitter.com/statuses/user_timeline/3175741.rss'
+
+# actual xml document
+document = xml.dom.minidom.parse(urllib.urlopen(URI))
+
+# create empty string to store information
+info = ""
+
+# parse feed elements
+for item in document.getElementsByTagName('item'):
+    title = item.getElementsByTagName('title')[0].firstChild.data
+    link = item.getElementsByTagName('link')[0].firstChild.data
+    description = item.getElementsByTagName('description')[0].firstChild.data
+    # format string as html and display
+    info_str = '''<li><a title="%s" href="%s">%s</a></li>\n''' % \
+        (title.encode('UTF8','replace'),
+        link.encode('UTF8', 'replace'),
+        description.encode('UTF8','replace'))
+    # concatenate all html into one string for printing
+    info += info_str
+
+# check result
+print "<ul>"
+print(info)
+print "</ul>"

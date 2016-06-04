bdd10fe369380f0f70543a21ea32187b2bbbff2a
Jason A. Clark <jasclark@gmail.com>
2016-06-04 00:49:39 -0600

    added decode content to avoid parse error

--- getFeed.py
+++ getFeed.py
@@ -4,33 +4,34 @@ from xml.etree.ElementTree import parse
 import requests
 import sys
 
-#URI of the feed
+# URI of the feed
 URI = sys.argv[1] if len(sys.argv) > 1 else 'http://www.npr.org/rss/rss.php?id=1019'
 
-#get feed data source
+# get feed data source
 request = requests.get(URI, stream=True)
 
-#check for HTTP codes other than 200
+# check for HTTP codes other than 200
 if request.status_code != 200:
     print('Status:', request.status_code, 'Problem with the request. Exiting.')
     exit()
 
-#parse data feed
+# parse data feed
+request.raw.decode_content = True
 data = parse(request.raw)
 
-#create empty string to store HTML
+# create empty string to store HTML
 document = ''
 
-#extract and output tags
+# extract and output tags
 for item in data.iterfind('channel/item'):
     title = item.findtext('title')
     description = item.findtext('description')
     date = item.findtext('pubDate')
     link = item.findtext('link')
-    markup = '<li><a href="'+link+'">'+title+ '-' +description+ '('+date+')</a></li>\n'
+    markup = '<li><a href="'+link+'">'+title+' - '+description+' ('+date+')</a></li>\n'
     document += markup
 
-#show result as HTML
+# show result as HTML
 print '''<ul>
 %s
 <ul>''' % document
